c053688fd4ede6ea5add00c9f8533024fc498216
Mohamed Hanafy <mohamed.hanfy.dev@outlook.com>
2021-05-17 22:40:02 +0200

    Add project files.

---
+++ Testfeat/Testfeat/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Testfeat
+{
+    public partial class MainPage : ContentPage
+    {
+        Page onbordingpage;
+        public MainPage()
+        {
+            InitializeComponent();
+            if(shouldShowOnboarding()==true)
+            {
+                App.Current.ModalPopping += Current_ModalPopping;
+                onbordingpage = new Onboarding();
+                Navigation.PushModalAsync(onbordingpage, false);
+            }
+        }
+
+        private void Current_ModalPopping(object sender, ModalPoppingEventArgs e)
+        {
+            if(e.Modal ==onbordingpage)
+            {
+                FadeBox.FadeTo(0, 1000);
+                onbordingpage =null;
+                App.Current.ModalPopping -= Current_ModalPopping;
+            }
+        }
+
+        private bool shouldShowOnboarding()
+        {
+            return true;
+        }
+    }
+}
---
+++ Testfeat/Testfeat/Onboarding.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Testfeat
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Onboarding : ContentPage
+    {
+        public Onboarding()
+        {
+            InitializeComponent();
+        }
+         protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            MyRadialGradient.RadiusX = width * 6;
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.PopModalAsync(false);
+        }
+    }
+}
